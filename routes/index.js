f1c89969021bb15cbe7dfcaade9b943ff186fc28
SungBin Hong <sungbinhong@SungBins-MacBook-Pro.local>
2019-02-15 16:17:57 -0500

    email section update

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', layout: 'index' });
 });
 
 router.get('/privacy', function (req, res, next) {
-    res.render('privacy', { title: 'nabiday privacy', layout: 'index'})
+    res.render('privacy', { title: 'nabiday privacy'})
+})
+
+router.post('/emailSubscribers', function (req, res, next) {
+    console.log(req.body)
+    res.render('thankyou', {title: 'Thank you for becoming our founding member'})
 })
 
 module.exports = router;
