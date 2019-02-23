0d9a216328f5dc1a365bb91b32c87ab53a2f0231
SungBin Hong <sungbinhong@SungBins-MacBook-Pro.local>
2019-02-22 22:23:07 -0500

    email function added

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ var router = express.Router();
 
 var mongoose = require('mongoose');
 
+
+var validator = require("email-validator");
+
+
 require('../models/userEmailModel');
 const UserEmail = mongoose.model('userEmails');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', layout: 'index' });
+  res.render('index', { title: 'Nabiday - The day you feel butterflies in your stomach', layout: 'index' });
 });
 
 router.get('/privacy', function (req, res, next) {
@@ -17,11 +21,18 @@ router.get('/privacy', function (req, res, next) {
 
 router.post('/emailSubscribers', function (req, res, next) {
     console.log(req.body)
-    const newEmail = new UserEmail({
-        email:req.body.email
-    })
-    newEmail.save()
-    res.render('thankyou', {title: 'Thank you for becoming our founding member'})
+
+    if (validator.validate(req.body.email)) {
+        const newEmail = new UserEmail({
+            email:req.body.email
+        })
+        newEmail.save()
+        res.render('thankyou', {title: 'Thank you for becoming our founding member'})
+    } else {
+        res.render('sorry', {title: 'Nabiday - The day you feel butterflies in your stomach'})
+    }
+
+
 })
 
 module.exports = router;
